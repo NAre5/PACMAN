82804d107f88411e1ba021b2414911c25c89dbfe
erant <sherlord5@gmail.com>
2019-05-06 01:40:32 +0300

    alpha

--- javascript.js
+++ javascript.js
@@ -39,7 +39,7 @@ bonus__eat_audio.loop = false;
 var die_audio = new Audio("audio/die.mp3");
 bonus__eat_audio.loop = false;
 var winning_audio = new Audio("audio/battleOver.mp3");
-winning_audio.loop = false;
+winning_audio.loop = true;
 
 
 
@@ -98,9 +98,7 @@ var DIRECTION = {
 };
 
 var pacman_direction;
-// var direction;
-// var direction = "right";
-// var direction = DIRECTION
+
 
 
 var max_balls = 90;
@@ -952,7 +950,7 @@ $("#settingsForm").submit(function (e) {
         for (var j = i + 1; j < cs.length; j++) {
             if (cs[i].value === cs[j].value) {
                 Swal.fire("colors must be different");
-                    return;
+                return;
             }
         }
     }
@@ -1023,24 +1021,6 @@ function guest_basic() {
     document.getElementById("game").hidden = false;
 }
 
-// Object.prototype.clone = function () {
-//     var i, newObj = (this instanceof Array) ? [] : {};
-//     for (i in this) {
-//         if (i === 'clone') {
-//             continue;
-//         }
-//         if (this[i] && typeof this[i] === "object") {
-//             newObj[i] = this[i].clone();
-//         } else {
-//             newObj[i] = this[i];
-//         }
-//     }
-//     return newObj;
-// };
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 function startNewGame() {
     pressedOnce = false;
     eaten50Points = false;
@@ -1106,84 +1086,7 @@ function roundRect(ctx, x, y, width, height, radius, fill, stroke, fillStyle) {
     }
 
 }
-// function drawClock() {
-//     var canvas = document.getElementById("canvas");
-//     var ctx = canvas.getContext("2d");
-//     var radius = canvas.height / 2;
-//     ctx.translate(radius, radius);
-//     radius = radius * 0.90
-//     setInterval(drawClock, 1000);
-//     drawFace(ctx, radius);
-//     drawNumbers(ctx, radius);
-//     drawTime(ctx, radius);
-// }
 
-// function drawFace(ctx, radius) {
-//     var grad;
-//     ctx.beginPath();
-//     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-//     ctx.fillStyle = 'white';
-//     ctx.fill();
-//     grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-//     grad.addColorStop(0, '#333');
-//     grad.addColorStop(0.5, 'white');
-//     grad.addColorStop(1, '#333');
-//     ctx.strokeStyle = grad;
-//     ctx.lineWidth = radius * 0.1;
-//     ctx.stroke();
-//     ctx.beginPath();
-//     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-//     ctx.fillStyle = '#333';
-//     ctx.fill();
-// }
-
-// function drawNumbers(ctx, radius) {
-//     var ang;
-//     var num;
-//     ctx.font = radius * 0.15 + "px arial";
-//     ctx.textBaseline = "middle";
-//     ctx.textAlign = "center";
-//     for (num = 1; num < 13; num++) {
-//         ang = num * Math.PI / 6;
-//         ctx.rotate(ang);
-//         ctx.translate(0, -radius * 0.85);
-//         ctx.rotate(-ang);
-//         ctx.fillText(num.toString(), 0, 0);
-//         ctx.rotate(ang);
-//         ctx.translate(0, radius * 0.85);
-//         ctx.rotate(-ang);
-//     }
-// }
-
-// function drawTime(ctx, radius) {
-//     var now = new Date();
-//     var hour = now.getHours();
-//     var minute = now.getMinutes();
-//     var second = now.getSeconds();
-//     //hour
-//     hour = hour % 12;
-//     hour = (hour * Math.PI / 6) +
-//         (minute * Math.PI / (6 * 60)) +
-//         (second * Math.PI / (360 * 60));
-//     drawHand(ctx, hour, radius * 0.5, radius * 0.07);
-//     //minute
-//     minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-//     drawHand(ctx, minute, radius * 0.8, radius * 0.07);
-//     // second
-//     second = (second * Math.PI / 30);
-//     drawHand(ctx, second, radius * 0.9, radius * 0.02);
-// }
-
-// function drawHand(ctx, pos, length, width) {
-//     ctx.beginPath();
-//     ctx.lineWidth = width;
-//     ctx.lineCap = "round";
-//     ctx.moveTo(0, 0);
-//     ctx.rotate(pos);
-//     ctx.lineTo(0, -length);
-//     ctx.stroke();
-//     ctx.rotate(-pos);
-// }
 
 function showAbout() {
     document.getElementById("About").showModal();
@@ -1212,7 +1115,6 @@ function changeSettings(){
 var scroll_flag=true;
 
 function setScroll(){
-    // document.getElementsByTagName("html")[0].overflow= scroll_flag?"scroll":"hidden";
     document.body.style.overflow= scroll_flag?"scroll":"hidden";
     scroll_flag= !scroll_flag;
     document.getElementById("scroll_f").innerHTML = (scroll_flag?"dis":"en")+"able";
